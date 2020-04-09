aaeb9b7493f931eeecc24d2d3388b679806b5745
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-10 00:02:54 +0800

    [Michelle] ST5 AC1 - should_park_car_in_first_parking_lot_with_equal_available_ratio_when_park

--- src/test/java/com/oocl/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/SuperSmartParkingBoyTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class SuperSmartParkingBoyTest {
 
     @Test
-    public void should_park_car_in_parking_lot_with_more_space_when_park() {
+    public void should_park_car_in_parking_lot_with_more_larger_available_ratio_when_park() {
         Car car = new Car();
         ParkingLot firstParkingLot = new ParkingLot(2);
         ParkingLot secondParkingLot = new ParkingLot(3);
@@ -21,5 +21,21 @@ public class SuperSmartParkingBoyTest {
         ParkingTicket actualPackingTicket = superSmartParkingBoy.park(car);
 
         Assert.assertEquals(secondParkingLot, actualPackingTicket.getParkingLot());
+    }
+
+    @Test
+    public void should_park_car_in_first_parking_lot_with_equal_available_ratio_when_park() {
+        Car car = new Car();
+        ParkingLot firstParkingLot = new ParkingLot(2);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        ParkingTicket firstParkingTicket = new ParkingTicket(firstParkingLot);
+        ParkingTicket secondParkingTicket = new ParkingTicket(secondParkingLot);
+        firstParkingLot.park(car, firstParkingTicket);
+        secondParkingLot.park(car, secondParkingTicket);
+
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(Arrays.asList(firstParkingLot, secondParkingLot));
+        ParkingTicket actualPackingTicket = superSmartParkingBoy.park(car);
+
+        Assert.assertEquals(firstParkingLot, actualPackingTicket.getParkingLot());
     }
 }
