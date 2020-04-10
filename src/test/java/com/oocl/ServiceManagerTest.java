6f5122189c323ed4423df5937f8ad5b0f674567c
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-10 21:57:45 +0800

    [Michelle] ST6 AC2 - should pass same testcase on ST3

--- src/test/java/com/oocl/ServiceManagerTest.java
+++ src/test/java/com/oocl/ServiceManagerTest.java
@@ -1,15 +1,26 @@
 package com.oocl;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class ServiceManagerTest {
 
+    private ParkingLot parkingLot;
+    private Car car;
+
+    @Before
+    public void setup() {
+        parkingLot = new ParkingLot();
+        car = new Car();
+    }
+
     @Test
     public void should_add_parking_boy_to_management_list_when_assign(){
-        ParkingLot parkingLot = new ParkingLot();
         ServiceManager serviceManager = new ServiceManager(Arrays.asList(parkingLot));
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot));
 
@@ -21,10 +32,8 @@ public class ServiceManagerTest {
 
     @Test
     public void should_assign_parking_boy_to_park_car_given_that_boy_is_on_management_list(){
-        ParkingLot parkingLot = new ParkingLot();
         ServiceManager serviceManager = new ServiceManager(Arrays.asList(parkingLot));
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot));
-        Car car = new Car();
 
         serviceManager.assignParkingBoyToManagementList(parkingBoy);
         ParkingTicket parkingTicket = serviceManager.assignParkingBoyToPark(parkingBoy, car);
@@ -34,10 +43,8 @@ public class ServiceManagerTest {
 
     @Test
     public void should_not_assign_parking_boy_to_park_car_given_that_boy_is_not_on_management_list(){
-        ParkingLot parkingLot = new ParkingLot();
         ServiceManager serviceManager = new ServiceManager(Arrays.asList(parkingLot));
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot));
-        Car car = new Car();
 
         ParkingTicket parkingTicket = serviceManager.assignParkingBoyToPark(parkingBoy, car);
 
@@ -46,10 +53,8 @@ public class ServiceManagerTest {
 
     @Test
     public void should_assign_parking_boy_to_fetch_car_given_that_boy_is_on_management_list(){
-        ParkingLot parkingLot = new ParkingLot();
         ServiceManager serviceManager = new ServiceManager(Arrays.asList(parkingLot));
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot));
-        Car car = new Car();
 
         serviceManager.assignParkingBoyToManagementList(parkingBoy);
         ParkingTicket parkingTicket = serviceManager.assignParkingBoyToPark(parkingBoy, car);
@@ -60,14 +65,38 @@ public class ServiceManagerTest {
 
     @Test
     public void should_not_assign_parking_boy_to_fetch_car_given_that_boy_is_not_on_management_list(){
-        ParkingLot parkingLot = new ParkingLot();
         ServiceManager serviceManager = new ServiceManager(Arrays.asList(parkingLot));
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot));
-        Car car = new Car();
 
         ParkingTicket parkingTicket = serviceManager.assignParkingBoyToPark(parkingBoy, car);
         Car fetchedCar = serviceManager.assignParkingBoyToFetch(parkingBoy, parkingTicket);
 
         Assert.assertNull(fetchedCar);
+    }
+
+    @Test
+    public void should_park_car_to_parking_lot() {
+        List<ParkingLot> parkingLotList = Collections.singletonList(parkingLot);
+        ServiceManager serviceManager = new ServiceManager(parkingLotList);
+
+        ParkingTicket parkingTicket = serviceManager.park(car);
+        Car fetchedCarFromParkingLot = parkingTicket.getParkingLot().fetch(parkingTicket);
+
+        Assert.assertEquals(fetchedCarFromParkingLot, car);
+    }
+
+    @Test
+    public void should_fetch_car_when_fetch_from_parking_lot_with_correct_ticket() {
+        //given
+        List<ParkingLot> parkingLotList = Collections.singletonList(parkingLot);
+        ServiceManager serviceManager = new ServiceManager(parkingLotList);
+        Car car = new Car();
+        ParkingTicket parkingTicket = new ParkingTicket(parkingLot);
+
+        parkingLot.park(car, parkingTicket);
+        //when
+        Car fetchedCarFromParkingLot = serviceManager.fetch(parkingTicket);
+        //then
+        Assert.assertEquals(fetchedCarFromParkingLot, car);
     }
 }
