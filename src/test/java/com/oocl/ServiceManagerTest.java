114a87af315d719c3b08f8586f4088920f6490d5
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-13 16:21:04 +0800

    [Michelle] ST6 AC1 - should_throw_exception_given_that_boy_on_management_list_is_not_available_to_park_car

--- src/test/java/com/oocl/ServiceManagerTest.java
+++ src/test/java/com/oocl/ServiceManagerTest.java
@@ -47,15 +47,17 @@ public class ServiceManagerTest {
         Assert.assertNotNull(parkingTicket);
     }
 
-//    @Test
-//    public void should_not_assign_parking_boy_to_park_car_given_that_boy_is_not_on_management_list(){
-//        ServiceManager serviceManager = new ServiceManager(Collections.singletonList(parkingLot));
-//        ParkingBoy parkingBoy = new ParkingBoy(Collections.singletonList(parkingLot));
-//
-//        ParkingTicket parkingTicket = serviceManager.assignParkingBoyToPark(car);
-//
-//        Assert.assertNull(parkingTicket);
-//    }
+    @Test
+    public void should_throw_exception_given_that_boy_on_management_list_is_not_available_to_park_car(){
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage(ErrorMsg.ERROR_MSG_OF_NOT_ENOUGH_POSITION);
+
+        ServiceManager serviceManager = new ServiceManager(Collections.singletonList(parkingLot));
+        ParkingBoy parkingBoy = new ParkingBoy(Collections.singletonList(new ParkingLot(1)));
+        parkingBoy.park(car);
+
+        serviceManager.assignParkingBoyToPark(car);
+    }
 
     @Test
     public void should_assign_parking_boy_to_fetch_car_given_that_boy_is_on_management_list(){
