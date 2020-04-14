50990169d91e6f45d10aed52e049d1f1387715de
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-14 16:53:17 +0800

    [Michelle] remove variable availableSpace in ParkingLot

--- src/main/java/com/oocl/ParkingLot.java
+++ src/main/java/com/oocl/ParkingLot.java
@@ -7,26 +7,22 @@ public class ParkingLot {
     public static final int DEFAULT_CAPACITY_10 = 10;
     private Map<ParkingTicket, Car> parkingTicketCarMap = new HashMap<>();
     private int capacity;
-    private int availableSpace;
     private float availablePositionRate;
 
     public ParkingLot() {
         this.capacity = DEFAULT_CAPACITY_10;
-        this.availableSpace = DEFAULT_CAPACITY_10;
         this.availablePositionRate = 1f;
     }
 
     public ParkingLot(int parkingLotSize) {
         this.capacity = parkingLotSize;
-        this.availableSpace = parkingLotSize;
         this.availablePositionRate = 1f;
     }
 
     public void park(Car car, ParkingTicket parkingTicket) {
         if (!isFull() && car != null) {
             parkingTicketCarMap.put(parkingTicket, car);
-            availableSpace = capacity - parkingTicketCarMap.size();
-            availablePositionRate = (float) availableSpace / (float) capacity;
+            availablePositionRate = (float) getAvailableSpace() / (float) capacity;
         }
     }
 
@@ -35,11 +31,11 @@ public class ParkingLot {
     }
 
     public boolean isFull() {
-        return availableSpace == 0;
+        return capacity - parkingTicketCarMap.size() == 0;
     }
 
     public int getAvailableSpace() {
-        return availableSpace;
+        return capacity - parkingTicketCarMap.size();
     }
 
     public float getAvailablePositionRate() {
